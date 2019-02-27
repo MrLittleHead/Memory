3828c9f57f8e58ae9735cba8f3d0cac5b579ace0
epoch <epoch@carmic-epoch>
2019-02-27 20:46:10 +0100

    Clean de fioritures et modification du Scan

--- src/memoryControleur/MemoryProgram.java
+++ src/memoryControleur/MemoryProgram.java
@@ -13,12 +13,12 @@ public class MemoryProgram {
 
 	private PlayerBo currentPlayer = null;
 	private GameBo currentGame = null;
-	private ScorePlayerBo currentScore ;
+	private ScorePlayerBo currentScore;
+	private Scanner scanGame = new Scanner(System.in);
 	
 	private int nbFindCard = 0;
 	private int nbPlayer;
 	private int nbCard;
-	
 	
 	private List<PlayerBo> listPlayerBo = new ArrayList<PlayerBo>(nbPlayer);
 	private List<ScorePlayerBo> listScore = new ArrayList<ScorePlayerBo>(nbPlayer);
@@ -28,7 +28,7 @@ public class MemoryProgram {
 	//je suis pas mal embrouiller avec les scanners j'ai du transformer mes string en scanner dans mes constructeur :/
 	//le code casse lors de la creation des joueurs : 
 	//mes intentions =  - creer une liste de joueurs 
-					//	- creer une liste de score associé (un tableau serai sans doute mieux) :s
+					//	- creer une liste de score associï¿½ (un tableau serai sans doute mieux) :s
 					//	- on passe au joueur et score suivant de la liste en cas d'echec findCard
 					// il manque les conditions de victoire et je dois refaire le CRUD + BD (Anglais oblige)
 	//a terme je veux supprimer les scan et faire une IHM
@@ -36,10 +36,9 @@ public class MemoryProgram {
 	private void gameCreation() {
 
 		System.out.println("entrez un nom de partie :");
-		Scanner scanNameGame = new Scanner(System.in);
-		scanNameGame.next();
+		scanGame.next();
 
-		GameBo game = new GameBo(scanNameGame);
+		GameBo game = new GameBo(scanGame);
 
 		listGame.add(game);
 		currentGame = game;
@@ -48,76 +47,67 @@ public class MemoryProgram {
 	private void playerCreation() {
 
 		System.out.print("Saisissez votre nombre de Joueurs : ");
-		Scanner scanNbPlayer = new Scanner(System.in);
 
-		while (!scanNbPlayer.hasNextInt()) 
+		while (!scanGame.hasNextInt())
 		{
-			System.out.print( " Caractère inconnu, rechoisissez un nombre de joueurs  : ");
-			scanNbPlayer.next();
+			System.out.print( " Caractï¿½re inconnu, rechoisissez un nombre de joueurs  : ");
+			scanGame.nextInt();
 		}
 
-		nbPlayer =	scanNbPlayer.nextInt();
-
+		nbPlayer = scanGame.nextInt();
+		Scanner scanPseudoPlayer = new Scanner(System.in);
 		for (int i = 0; i < nbPlayer; i++) {
-			System.out.print("Saisissez le nom du player" + i +" :");
-			Scanner scanPseudoPlayer = new Scanner(System.in);
+			System.out.print("Saisissez le nom du player " + i + " :");
+			scanPseudoPlayer.next();
 			PlayerBo pseudo = new PlayerBo(scanPseudoPlayer);
 			listPlayerBo.add(pseudo);
 			ScorePlayerBo score = new ScorePlayerBo(pseudo, currentGame.getId_Game(), 0);
 			listScore.add(score);
 		}
-		
 		currentPlayer = listPlayerBo.get(0);
 		currentScore  = listScore.get(0);
-		scanNbPlayer.close();
-		
 	}
 
-	private int listeCardCreation() {
+	private void listeCardCreation() {
 
 		System.out.print("Saisissez votre nombre de cartes : ");
-		Scanner scanNbCard = new Scanner(System.in);
-		while (!scanNbCard.hasNextInt()) 
+		while (!scanGame.hasNextInt())
 		{
-			System.out.print( " Caractère inconnu, rechoisissez une premiere carte  : ");
-			scanNbCard.next();
+			System.out.print( " Caractï¿½re inconnu, rechoisissez votre nombre de cartes  : ");
+			scanGame.nextInt();
 		}
-		nbCard =	scanNbCard.nextInt(); 
-		while (nbCard % 2 == 1) 
+		while (nbCard % 2 == 1)
 		{
 			System.out.println("Nombre de Cartes invalide (impaire)!");
 			System.out.print("Ressaisissez votre nombre de cartes : ");
-			while (!scanNbCard.hasNextInt()) 
+			while (!scanGame.hasNextInt())
 			{
-				System.out.print(currentPlayer + "Caractère inconnu, rechoisissez votre nombre de cartes  : ");
-				scanNbCard.next();
+				System.out.print(currentPlayer + "Caractï¿½re inconnu, rechoisissez votre nombre de cartes  : ");
+				scanGame.nextInt();
 			}
-			nbCard =	scanNbCard.nextInt();
-
+			nbCard = scanGame.nextInt();
 		}
-		return nbCard;
 	}
 	private int ChooseCard(PlayerBo player, DistributionBo listCard) 
 	{
 		int choosenCard;
 		
 		System.out.print(player.getPseudo() + " Choisissez une carte : ");
-		Scanner scanChooseCard = new Scanner(System.in);
-		while (!scanChooseCard.hasNextInt()) 
+		while (!scanGame.hasNextInt()) 
 		{
-			System.out.print(player.getPseudo() + " Caractère inconnu, rechoisissez une premiere carte  : ");
-			scanChooseCard.next();
+			System.out.print(player.getPseudo() + " Caractï¿½re inconnu, rechoisissez une premiere carte  : ");
+			scanGame.next();
 		}
-		choosenCard =	scanChooseCard.nextInt() - 1;
+		choosenCard = scanGame.nextInt() - 1;
 		while (choosenCard >= nbCard || choosenCard < 0 || listCard.get(choosenCard).isVisible() ) 
 		{
 			System.out.print(player.getPseudo() + "Numero de carte invalide, rechoisissez de nouveau carte : ");
-			while (!scanChooseCard.hasNextInt()) 
+			while (!scanGame.hasNextInt()) 
 			{
-				System.out.print(player.getPseudo() + " Caractère inconnu, rechoisissez de nouveau une carte  : ");
-				scanChooseCard.next();
+				System.out.print(player.getPseudo() + " Caractï¿½re inconnu, rechoisissez de nouveau une carte  : ");
+				scanGame.next();
 			}
-			choosenCard =	scanChooseCard.nextInt() - 1; // -1 car pour plus de facilité pour choisir la carte 0 on designe la carte 1
+			choosenCard =	scanGame.nextInt() - 1; // -1 car pour plus de facilitï¿½ pour choisir la carte 0 on designe la carte 1
 		}
 		return choosenCard;
 	}
@@ -126,7 +116,7 @@ public class MemoryProgram {
 	{
 		int choosenCard = 0;
 		int choosenCard2 = 0;
-		
+
 		for (int i = 0; i <= 1; i++) {
 			if (i == 0) {
 				choosenCard = ChooseCard(currentPlayer, listCard);
@@ -139,29 +129,23 @@ public class MemoryProgram {
 				System.out.println(listCard.toString());
 			}
 		}
-
 		if (! listCard.get(choosenCard).equals(listCard.get(choosenCard2))) 
 		{
 			listCard.get(choosenCard).setVisible(false);
 			listCard.get(choosenCard2).setVisible(false);
-
 			currentPlayer = listPlayerBo.get(+1);	// a Tester vraiment pas sur de moi j'essaye de faire passer au joueur next
 			currentScore = listScore.get(+1);		//idem pour le score
-			
 			if (currentPlayer == listPlayerBo.get(nbPlayer + 1)) {
-				currentPlayer = listPlayerBo.get(1);		
+				currentPlayer = listPlayerBo.get(1);
 			}
 		}
-		else 
+		else
 		{
 			currentScore.findPairCard();
 			System.out.println("Total Score " + currentPlayer + " : " + currentScore.getScore());
 			nbFindCard += 2;
 		}
 	}
-
-
-
 
 	public MemoryProgram() 
 	{
@@ -188,7 +172,6 @@ public class MemoryProgram {
 		{
 			System.out.println("Match NUL ! ");
 		}*/
-
+		scanGame.close();
 	}
-
 }
